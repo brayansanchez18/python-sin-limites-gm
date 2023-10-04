abef7d0df761ebec916f31bec3e4a3bee6136b76
brayan emanuel sanchez ramirez <brayan.sanchez.contacto@gmail.com>
2023-10-04 12:46:39 -0600

    tipos de datos y entrada de datos en python

---
+++ Leccion01/03-tipos-de-datos/main.py
@@ -0,0 +1,61 @@
+x = 10
+print(x)
+# para saber el tipo de dato que almacena una variable type()
+print(type(x))
+print('----------------------------------------------------------------')
+# tipos de datos
+#int
+x = 10
+print(x)
+print(type(x))
+
+# tipo float
+#float
+x = 8.12
+print(x)
+print(type(x))
+
+#string
+#str
+x = 'hola mundo'
+print(x)
+print(type(x))
+
+#boolean
+#bool
+# x = True
+x = False
+print(x)
+print(type(x))
+
+# manejo de cadenas (concatenacion)
+miGrupoFavorito = 'Guns and Roses'
+print("mi grupo favorito es: " + miGrupoFavorito)
+print("mi grupo favorito es:", miGrupoFavorito)
+
+n1 = 5
+n2 = 4
+print("suma:", int(n1) + int(n2))
+
+# Tipos bool (boolean)
+miVariable = False
+print(miVariable)
+
+miVariable = 3 < 2
+print(miVariable)
+
+if miVariable:
+    print("El resultado fue verdadero")
+else:
+    print("El resultado fue falso")
+
+#input para procesar entrada de datos
+# result = input('escribe un mensaje: ')
+# print("mensaje: " + result)
+# print('fin del programa')
+
+# funcion input para procesar la entrada de un usuario
+# numero1 = int(input("Escribe el primer número: "))
+# numero2 = int(input("Escribe el segundo número: "))
+# resultado = numero1 + numero2
+# print("El resultado de la suma es:", resultado)
---
+++ Leccion01/03ejercicio/main.py
@@ -0,0 +1,13 @@
+# calificar tu dia
+# resultado = int(input('como estuvo tu dia del 1 al 10?: '))
+# print('mi dia estuvo de:', resultado)
+
+# se solicita incluir la siguiente infomacion acerca de un libro
+# titulo
+# autor
+# y mandar el siguiente mensaje a consola
+# $titulo fue escrito por $autor
+
+titulo = input('proporciona el titulo del libro: ')
+autor = input('porporciona el autor del libro: ')
+print("%s fue escrito por %s" %(titulo, autor))
