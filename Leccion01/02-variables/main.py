1acc1a4df10e171d21cc6c1787964db6eae8b289
brayan emanuel sanchez ramirez <brayan.sanchez.contacto@gmail.com>
2023-10-04 12:00:44 -0600

    variables en python

---
+++ Leccion01/02-variables/main.py
@@ -0,0 +1,7 @@
+miVariable = 'hola mundo desde python'
+print(miVariable)
+
+n1 = 5
+n2 = 5
+res = n1+n2
+print("resultado de %i + %i = %i"%(n1, n2, res))
---
+++ Leccion01/02ejercicio/main.py
@@ -0,0 +1,7 @@
+nombre = "Brayan Emaneul"
+telefono = 7295924900
+email = "bs6961024@gmail.com"
+
+print(nombre)
+print(telefono)
+print(email)
